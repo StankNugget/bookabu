6cefbf5ddb62f1e3f8bfa359dc2d8259238f8816
Alejandro Gonzalez <127909380+StankNugget@users.noreply.github.com>
2023-03-18 22:17:09 +0000

    Made it run properly

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import openai
 import os
-from flask import Flask, request, render_template
+import markdown
+import logging
+from flask import Flask, request, render_template, Markup
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, SubmitField
 
 app = Flask(__name__, template_folder='templates')
-app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SECRET_KEY'] = '\xa9G:-\x9c\xc4\xa4\x85\xa5\x18\xd8UL\x84\xc6\xdd\x18\xbd2:F\xe3\x91'
 csrf = CSRFProtect(app)
 
-openai.api_key = "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"
+logging.basicConfig(level=logging.DEBUG)
 
-messages = [{"role": "system", "content": "You are a financial expert that specializes in real estate investment and negotiation"}]
+openai.api_key = "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"
+
+messages = [{"role": "system", "content": "You are a real estate guru"}]
 
 class InputForm(FlaskForm):
     user_input = StringField('Your question:')
@@ -32,8 +36,10 @@ def index():
     response = None
     if form.validate_on_submit():
         user_input = form.user_input.data
-        response = CustomChatGPT(user_input)
+        response_text = CustomChatGPT(user_input)
+        response = Markup(markdown.markdown(response_text))  # Convert the response to HTML using markdown
     return render_template('index.html', form=form, response=response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 80)))
+
